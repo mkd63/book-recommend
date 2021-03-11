26412a0663df2fe4b78c4652dcafbf7b4402598c
mkd63 <sharmaaahan15@gmail.com>
2021-03-11 13:47:25 +0530

    added login register to router

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { GlobalProvider } from "../context/GlobalContext";
 import Authentication from "../components/pages/Authentication";
+import Login from "../components/pages/Login";
+import Register from "../components/pages/Register";
 
 // AppRouter Component
 export default function AppRouter() {
@@ -10,8 +12,8 @@ export default function AppRouter() {
       <GlobalProvider>
         <Switch>
           <Route exact path="/" component={Authentication} />
-          <Route exact path="/login" component={Authentication} />
-          <Route exact path="/register" component={Authentication} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
         </Switch>
       </GlobalProvider>
     </BrowserRouter>
