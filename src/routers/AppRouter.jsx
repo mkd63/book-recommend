ddbd780683f5771aa99637b711d53bede04b80ca
mkd63 <sharmaaahan15@gmail.com>
2021-03-17 00:42:04 +0530

    updated app router

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,6 +4,7 @@ import { GlobalProvider } from "../context/GlobalContext";
 import Authentication from "../components/pages/Authentication";
 import Login from "../components/pages/Login";
 import Register from "../components/pages/Register";
+import AdminForm from "../components/pages/AdminForm";
 
 // AppRouter Component
 export default function AppRouter() {
@@ -14,6 +15,7 @@ export default function AppRouter() {
           <Route exact path="/" component={Authentication} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/admin" component={AdminForm} />
         </Switch>
       </GlobalProvider>
     </BrowserRouter>
