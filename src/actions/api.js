1a4bcab180ec7d25c11279e82b1040fb1cbfacc9
mkd63 <sharmaaahan15@gmail.com>
2021-03-17 00:43:35 +0530

    update authentication and api.js

--- src/actions/api.js
+++ src/actions/api.js
@@ -3,11 +3,30 @@ export async function GET(endpoint, token) {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}${endpoint}/`,
       {
-        method: 'GET',
+        method: "GET",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: token ? `JWT ${token}` : '',
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token ? `JWT ${token}` : "",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function GETSEARCH(endpoint, token) {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}${endpoint}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token ? `JWT ${token}` : "",
         },
       }
     );
@@ -22,11 +41,11 @@ export async function POST(endpoint, body, token) {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}${endpoint}/`,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: token ? `JWT ${token}` : '',
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token ? `JWT ${token}` : "",
         },
         body: JSON.stringify(body),
       }
@@ -42,11 +61,11 @@ export async function PATCH(endpoint, body, token) {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}${endpoint}/`,
       {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: token ? `JWT ${token}` : '',
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token ? `JWT ${token}` : "",
         },
         body: JSON.stringify(body),
       }
@@ -62,11 +81,11 @@ export async function DELETE(endpoint, token) {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}${endpoint}/`,
       {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: token ? `JWT ${token}` : '',
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token ? `JWT ${token}` : "",
         },
       }
     );
--- src/components/pages/Authentication.jsx
+++ src/components/pages/Authentication.jsx
@@ -1,28 +1,99 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
-import { Grid, Box, Hidden, Typography } from "@material-ui/core";
+import { Grid, Box, Hidden, Typography, Button } from "@material-ui/core";
 import { GlobalContext } from "../../context/GlobalContext";
 import { Link } from "@material-ui/core";
 import Home from "./Home";
+import bookLogo from "../../assets/book.png";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { POST } from "../../actions/api";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+
 export default function Authentication(props) {
   const classes = useStyles();
-  const { session } = useContext(GlobalContext);
+  const { session, setSession } = useContext(GlobalContext);
 
   const { from } = props.location.state || {
     from: { pathname: `/${session.username}` },
   };
   const preventDefault = (event) => event.preventDefault();
-
+  const [bookData, setBookData] = useState("");
   const loggedIn = (username) => {
     props.history.push({
       pathname: `/${username}`,
     });
   };
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogout = async () => {
+    handleClose();
+    const response = await POST("/auth/logout");
+    if (response.status === 200) {
+      setSession({
+        token: null,
+        userId: null,
+        userFirstName: null,
+        userLastName: null,
+        userImg: null,
+      });
+    }
+  };
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      style={{ top: -295 }}
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+    </Menu>
+  );
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY, process.env.REACT_APP_API_URL);
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=harry&key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setBookData(result.items);
+      });
+  }, []);
   return (
     <div className={classes.root}>
       <div className={classes.header}>
-        <h3 className={classes.logoText}>MyBooks</h3>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img src={bookLogo} style={{ height: 35, width: 35 }}></img>
+          <h3 className={classes.logoText}>MyBooks</h3>
+        </div>
         <div className={classes.options}>
           <Link
             className={classes.optionText}
@@ -49,34 +120,48 @@ export default function Authentication(props) {
           >
             About
           </Link>
-
-          <Link
-            className={classes.optionText}
-            href="/login"
-            onClick={preventDefault}
-            color="inherit"
-          >
-            Login
-          </Link>
-          <div
-            style={{
-              width: 0.5,
-              height: 23,
-              background: "rgba(0,0,0,0.54)",
-              marginTop: 2,
-              marginLeft: 6,
-              marginRight: 6,
-            }}
-          />
-          <Link
-            className={classes.optionText}
-            style={{ marginLeft: 0 }}
-            href="/register"
-            onClick={preventDefault}
-            color="inherit"
-          >
-            Register
-          </Link>
+          {!session.token ? (
+            <div className={classes.options}>
+              <Link
+                className={classes.optionText}
+                href="/login"
+                color="inherit"
+              >
+                Login
+              </Link>
+              <div
+                style={{
+                  width: 0.5,
+                  height: 23,
+                  background: "rgba(0,0,0,0.54)",
+                  marginTop: 2,
+                  marginLeft: 6,
+                  marginRight: 6,
+                }}
+              />
+              <Link
+                className={classes.optionText}
+                style={{ marginLeft: 0 }}
+                href="/register"
+                color="inherit"
+              >
+                Register
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Button
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={() => setAnchorEl(true)}
+                startIcon={<AccountCircleIcon />}
+                style={{ marginLeft: 14 }}
+              >
+                {session.userFirstName + " " + session.userLastName}
+              </Button>
+              {renderMenu}
+            </div>
+          )}
         </div>
       </div>
       <Route exact path="/" component={Home} />
@@ -89,6 +174,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   header: {
+    zIndex: 2000,
     backgroundColor: "#FFF",
     width: "100%",
     padding: 15,
@@ -101,8 +187,10 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
   },
   logoText: {
+    marginTop: 0,
+    marginBottom: 0,
+    fontFamily: "cursive",
     fontSize: 16,
-    lineHeight: 20,
     color: "#21ff00",
   },
   boxPrimary: {
