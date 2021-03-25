ddd972439e2ace2ceddf5784ada74e1e84aeadd0
mkd63 <sharmaaahan15@gmail.com>
2021-03-25 15:31:23 +0530

    add footer ui

--- src/components/uiComponents/Footer.jsx
+++ src/components/uiComponents/Footer.jsx
@@ -8,8 +8,11 @@ import {
   Typography,
   Button,
   Paper,
+  IconButton,
+  Tooltip,
 } from "@material-ui/core";
-
+import GitHubIcon from "@material-ui/icons/GitHub";
+import bookLogo from "../../assets/book.png";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -28,14 +31,100 @@ import StarIcon from "@material-ui/icons/Star";
 
 export default function Footer(props) {
   return (
-    <Grid
-      container
+    <div
       style={{
         width: "100%",
-        height: 250,
-        backgroundColor: "rgba(255, 140, 0, 0.64)",
+        height: 150,
+        backgroundColor: "#fff",
       }}
-    ></Grid>
+    >
+      <div
+        style={{
+          width: "100%",
+          height: 150,
+          background: "rgb(48, 63, 159,1)",
+          boxShadow: "0 3px 5px 2px rgba(0,0,0, .3)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          flexWrap: "wrap-reverse",
+        }}
+      >
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexDirection: "column",
+            width: "15%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              paddingLeft: 20,
+            }}
+          >
+            <img src={bookLogo} style={{ height: 55, width: 55 }} />
+            <h3
+              style={{
+                fontFamily: "Roboto",
+                fontWeight: "300",
+
+                marginTop: 0,
+                marginBottom: 0,
+                fontSize: 16,
+                color: "rgba(255, 140, 0, 1)",
+              }}
+            >
+              My Books
+            </h3>
+          </div>
+        </div>
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "70%",
+          }}
+        >
+          <Typography variant="body1" style={{ color: "#fff" }}>
+            {"© 2021 My Books All Rights Reserved "}{" "}
+            <Link
+              target="_blank"
+              href="https://www.privacypolicies.com/live/8ccca296-cb47-490a-aa96-f4109d7e0426"
+              style={{ color: "#fff" }}
+            >
+              Privacy Policy
+            </Link>
+          </Typography>
+        </div>
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "10%",
+          }}
+        >
+          <IconButton
+            target="_blank"
+            href={"https://github.com/mkd63/book-recommend"}
+          >
+            <Tooltip title="Github">
+              <GitHubIcon style={{ color: "#fff" }} />
+            </Tooltip>
+          </IconButton>
+        </div>
+      </div>
+    </div>
   );
 }
 
